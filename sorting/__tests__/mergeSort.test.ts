894b3ce9c322a82aec9d398beccc480ce4d981b0
Kevin Arbi <karbica@eml.cc>
2020-06-25 23:01:35 -0700

    feat: add merge sort

---
+++ sorting/__tests__/mergeSort.test.ts
@@ -0,0 +1,9 @@
+import * as test from "tape";
+import mergeSort from "../mergeSort";
+
+test("Insertion Sort", (t) => {
+    const input = [3, 10, 6, 2, 8, 6, 4, 3, 7, 3, 8, 9];
+    const output = mergeSort(input);
+    t.same(output, input.sort((a: number, b: number) => a - b));
+    t.end();
+});
---
+++ sorting/mergeSort.ts
@@ -0,0 +1,34 @@
+/**
+ * A Merge Sort implementation recursively breaks down the array
+ * until two elements of the internal arrays can be swapped. It
+ * is then rebuilt again into a sorted array. This implementation
+ * returns a new array.
+ * 
+ * @param array - The array to be sorted.
+ * @returns The array sorted as ascended.
+ */
+export default function mergeSort(array: number[]) {
+    if (array.length < 2) {
+        return array;
+    }
+
+    const middle = Math.floor(array.length / 2);
+    const left = array.slice(0, middle);
+    const right = array.slice(middle);
+
+    return merge(mergeSort(left), mergeSort(right));
+}
+
+function merge(left: number[], right: number[]) {
+    const sorted = [];
+
+    while (left.length && right.length) {
+        if (left[0] <= right[0]) {
+            sorted.push(left.shift());
+        } else {
+            sorted.push(right.shift());
+        }
+    }
+
+    return [...sorted, ...left, ...right];
+}
