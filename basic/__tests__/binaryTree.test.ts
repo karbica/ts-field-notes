41ebe62a72c74870ee592b0ddce25c9c6cba6176
Kevin Arbi <karbica@eml.cc>
2020-06-28 22:05:05 -0700

    style: fix hanging comma in binary tree test

--- basic/__tests__/binaryTree.test.ts
+++ basic/__tests__/binaryTree.test.ts
@@ -22,23 +22,23 @@ tape("Binary Tree Data Structure", (t) => {
         "c",
         "should correctly refer to root right value"
     );
-    btree.inOrder((node) => inOrderTraversal.push(node.value)),
-        t.same(
-            inOrderTraversal,
-            ["h", "d", "i", "b", "e", "a", "f", "c", "g"],
-            "should correctly traverse in-order"
-        );
-    btree.preOrder((node) => preOrderTraversal.push(node.value)),
-        t.same(
-            preOrderTraversal,
-            ["a", "b", "d", "h", "i", "e", "c", "f", "g"],
-            "should correctly traverse pre-order"
-        );
-    btree.postOrder((node) => postOrderTraversal.push(node.value)),
-        t.same(
-            postOrderTraversal,
-            ["h", "i", "d", "e", "b", "f", "g", "c", "a"],
-            "should correctly traverse post-order"
-        );
+    btree.inOrder((node) => inOrderTraversal.push(node.value));
+    t.same(
+        inOrderTraversal,
+        ["h", "d", "i", "b", "e", "a", "f", "c", "g"],
+        "should correctly traverse in-order"
+    );
+    btree.preOrder((node) => preOrderTraversal.push(node.value));
+    t.same(
+        preOrderTraversal,
+        ["a", "b", "d", "h", "i", "e", "c", "f", "g"],
+        "should correctly traverse pre-order"
+    );
+    btree.postOrder((node) => postOrderTraversal.push(node.value));
+    t.same(
+        postOrderTraversal,
+        ["h", "i", "d", "e", "b", "f", "g", "c", "a"],
+        "should correctly traverse post-order"
+    );
     t.end();
 });
