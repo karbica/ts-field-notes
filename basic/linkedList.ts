e293855320a6f2a6b866af468e2b1834e28dfbc7
Kevin Arbi <karbica@eml.cc>
2020-07-06 02:04:19 -0700

    chore: code formatter

--- basic/linkedList.ts
+++ basic/linkedList.ts
@@ -5,7 +5,7 @@ export default class LinkedList<T> {
 
     /**
      * Initializes a linked list from an array of values.
-     * 
+     *
      * @param values - An array of values to initialize an instance.
      * @returns An instance of a linked list.
      */
@@ -23,7 +23,7 @@ export default class LinkedList<T> {
      * A Linked List data structure stores values with pointers to the
      * next value in the series. This class will expose common methods
      * implemented by an Array.
-     * 
+     *
      * @class
      */
     constructor() {
@@ -34,7 +34,7 @@ export default class LinkedList<T> {
 
     /**
      * Appends a value to the end of the linked list.
-     * 
+     *
      * @param value - A value to append.
      * @returns The length of the linked list.
      */
@@ -64,7 +64,7 @@ export default class LinkedList<T> {
     /**
      * Removes the node at the end of the linked list and returns
      * that node.
-     * 
+     *
      * @returns The node removed from the linked list.
      */
     pop(): T {
@@ -79,6 +79,7 @@ export default class LinkedList<T> {
     /**
      * Iterates over every value in the linked list and invokes the
      * given function.
+     *
      * @param fn - The callback function to invoke.
      */
     forEach(fn: (node: Node<T>) => void): void {
@@ -91,7 +92,7 @@ export default class LinkedList<T> {
 
     /**
      * Finds a node in the linked list given the find function.
-     * 
+     *
      * @param fn - The find function to invoke.
      * @returns The callback function to determine the find decision.
      */
@@ -111,7 +112,7 @@ export default class LinkedList<T> {
 
     /**
      * Returns the node right before the tail.
-     * 
+     *
      * @returns The penultimate node.
      */
     penultimate(): Node<T> | null {
@@ -137,7 +138,7 @@ export class Node<T> {
     /**
      * A node within a linked list contains a stored value and a
      * pointer to the next node.
-     * 
+     *
      * @class
      */
     constructor(value: T) {
