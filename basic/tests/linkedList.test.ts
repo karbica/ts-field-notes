a8957db9fddbd0882be9eee2ae628e23b6375c35
Kevin Arbi <karbica@eml.cc>
2020-07-06 18:28:46 -0700

    test: changed order of statements in linked list test

--- basic/tests/linkedList.test.ts
+++ basic/tests/linkedList.test.ts
@@ -1,18 +1,26 @@
-import tape from '../../fixture';
 import sinon from 'sinon';
+import tape from '../../fixture';
 import LinkedList from '../linkedList';
 
 tape('Linked List Data Structure', (t) => {
-    let linkedList;
+    const spy = sinon.spy();
+    let linkedList: LinkedList<number>;
     const values = [...Array<number>(10).keys()];
-    t.doesNotThrow(() => linkedList = LinkedList.from<number>(values));
+    t.doesNotThrow(
+        () => (linkedList = LinkedList.from<number>(values)),
+        'should create a linked list from an array of values'
+    );
     t.equal(linkedList.head.value, 0, 'should correctly point to head value');
     t.equal(linkedList.tail.value, 9, 'should correctly point to tail value');
     t.equal(linkedList.length, 10, 'should have the correct length');
     t.equal(linkedList.pop(), 9, 'should correctly pop value');
     t.equal(linkedList.length, 9, 'should correctly have length after pop');
     t.equal(linkedList.push(9), 10, 'should correctly have length after push');
-    t.equal(linkedList.tail.value, 9, 'should correctly point to value after push');
+    t.equal(
+        linkedList.tail.value,
+        9,
+        'should correctly point to value after push'
+    );
     t.equal(
         linkedList.penultimate().value,
         8,
@@ -23,8 +31,11 @@ tape('Linked List Data Structure', (t) => {
         3,
         'should correctly find a node given a find function'
     );
-    const spy = sinon.spy();
     linkedList.forEach(spy);
-    t.equal(spy.callCount, linkedList.length, 'should correctly iterate over every node');
+    t.equal(
+        spy.callCount,
+        linkedList.length,
+        'should correctly iterate over every node'
+    );
     t.end();
 });
