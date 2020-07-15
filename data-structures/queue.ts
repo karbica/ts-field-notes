af06b13712d1266ec26cc2374c05190dae9cfb24
Kevin Arbi <karbica@eml.cc>
2020-07-15 14:48:23 -0700

    refactor: changed comments and variable names for queue and stack

--- data-structures/queue.ts
+++ data-structures/queue.ts
@@ -1,33 +1,53 @@
-/**
- * A Queue data structure implementation that allows values to be
- * accessed in a first-in first-out manner. The underlying data
- * structure is an array.
- * 
- * @returns An instance of a queue.
- */
 export default class Queue<T> {
     #queue: T[];
 
+    /**
+     * A queue allows data to be accessed in a first-in first-out
+     * manner with other methods to control the flow.
+     * @class
+     */
     constructor() {
         this.#queue = [];
     }
 
+    /**
+     * Adds an item to the back of the queue.
+     * @param item - Enqueues the given item.
+     * @returns The new length of the queue.
+     */
     enqueue(item: T): number {
         return this.#queue.push(item);
     }
 
+    /**
+     * Removes an item from the front of the queue.
+     * @returns The item removed from the front of the queue.
+     */
     dequeue(): T {
         return this.isEmpty() ? null : this.#queue.shift();
     }
 
+    /**
+     * Returns the item at the front of the queue without removing
+     * it from the queue.
+     * @returns The item at the front of the queue.
+     */
     peek(): T {
         return this.isEmpty() ? null : this.#queue[0];
     }
 
+    /**
+     * Checks whether or not the queue is empty.
+     * @returns A boolean indicating if the queue is empty.
+     */
     isEmpty(): boolean {
         return this.#queue.length === 0;
     }
 
+    /**
+     * The length or count of items in the queue.
+     * @returns The number of items in the queue.
+     */
     get length(): number {
         return this.#queue.length;
     }
--- data-structures/stack.ts
+++ data-structures/stack.ts
@@ -1,34 +1,54 @@
-/**
- * A Stack data structure implementation that allows values to be
- * accessed in first-in last-out manner. The underlying data
- * structure is an array.
- * 
- * @returns An instance of a stack.
- */
 export default class Stack<T> {
-    #queue: T[];
+    #stack: T[];
 
+    /**
+     * A stack allows items to be accessed in a first-in last-out
+     * manner with other methods to control the flow.
+     * @class
+     */
     constructor() {
-        this.#queue = [];
+        this.#stack = [];
     }
 
+    /**
+     * Adds an item to the top of the stack.
+     * @param item - Pushes the given item.
+     * @returns The new length of the stack.
+     */
     push(item: T): number {
-        return this.#queue.push(item);
+        return this.#stack.push(item);
     }
 
+    /**
+     * Removes an item from the top of the stack.
+     * @returns The item removed from the top of the stack.
+     */
     pop(): T {
-        return this.isEmpty() ? null : this.#queue.pop();
+        return this.isEmpty() ? null : this.#stack.pop();
     }
 
+    /**
+     * Returns the item at the top of the stack without removing it
+     * from the stack.
+     * @returns The item at the top of the stack.
+     */
     peek(): T {
-        return this.isEmpty() ? null : this.#queue[this.length - 1];
+        return this.isEmpty() ? null : this.#stack[this.length - 1];
     }
 
+    /**
+     * Checks whether or not the stack is empty.
+     * @returns A boolean indicating if the stack is empty.
+     */
     isEmpty(): boolean {
-        return this.#queue.length === 0;
+        return this.#stack.length === 0;
     }
 
+    /**
+     * The count of items in the stack.
+     * @returns The number of items in the stack.
+     */
     get length(): number {
-        return this.#queue.length;
+        return this.#stack.length;
     }
 }
