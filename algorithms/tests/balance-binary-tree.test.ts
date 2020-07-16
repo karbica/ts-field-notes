bf931194ff4e7f6626281dc8a8869914e1d0bdd2
Kevin Arbi <karbica@eml.cc>
2020-07-16 00:45:56 -0700

    test: add smoke test to balance binary tree, remove unused import

--- algorithms/tests/balance-binary-tree.test.ts
+++ algorithms/tests/balance-binary-tree.test.ts
@@ -1,11 +1,12 @@
 import tape from '../../fixture';
 import BinaryTree from '../../data-structures/binary-tree';
-import fn, { inOrder } from '../balance-binary-tree';
+import fn  from '../balance-binary-tree';
 
 tape('Balance Binary Tree', (t) => {
     const arr = [1, 5, 3, 6, 2, 8, 7, 9];
     const tree = BinaryTree.from(arr);
     // TODO Figure out how to test the balance.
+    t.doesNotThrow(() => fn(tree.root), 'balances a binary tree without error');
     t.end();
 });
 
